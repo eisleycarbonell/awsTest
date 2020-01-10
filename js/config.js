6b101ac382accbc0e69593a5a756cb47f0d63a62
eisley_dev <eisleyjscarbonell@gmail.com>
2020-01-10 17:07:20 +0000

    config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Ef8Z6wb2z', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '34n62puikfqt13g8tfu5tt28ub', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
