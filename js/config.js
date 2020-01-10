ffae6defd4379d4640112f8460d8293e5fd5b121
eisley_dev <eisleyjscarbonell@gmail.com>
2020-01-10 19:24:36 +0000

    config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_X6R5EedYs', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'tefkr2i2rcitmaa87lrug0ds8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'eu-west-1_CUUeCtIRO', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3fdjp68csb5fnbkuuh2nnbu4t4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
